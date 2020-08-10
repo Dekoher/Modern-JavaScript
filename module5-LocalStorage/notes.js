54876796d7697f4f7974deb1c4055a342a037bcb
Dekoher <diegoherr92@gmail.com>
2020-08-10 01:44:39 -0500

    finishing module5-LocalStorage

---
+++ module5-LocalStorage/notes.js
@@ -0,0 +1,20 @@
+// Local Storage
+
+// Ventaja - permite añadir cierta informacion en el navegador del usuario,
+// por ejemplo en un carrito de compras, al regresar el usuario podria ver sus articulos en el
+// y no tendira la necesidad de volcerlos a agregar
+
+// Add to local Storage
+// localStorage.setItem('name', 'Diego');
+
+// // Session Storage
+// // sessionStorage.setItem('name', 'Diego');
+
+// // delete from Local Storage
+// // localStorage.removeItem('name');
+
+// const name = localStorage.getItem('name');
+// console.log(name);
+
+// // clean all Local Storage
+// localStorage.clear();
